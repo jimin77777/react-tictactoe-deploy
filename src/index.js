d25e0d2ac73b11a5abc971ef4b826f6fcea25069
jimin <jimin@carvazo.com>
2023-08-02 16:19:43 +0900

    [jimin] manifest modify

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App  basename={process.env.PUBLIC_URL}/>
+    <App />
   </React.StrictMode>
 );
 
